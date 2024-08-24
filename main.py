6c8bc972399fcaa0009d3f08b869b189cd6f23b0
maliha shahi <maliha.shahi.03@gmail.com>
2024-08-24 22:38:26 +0430

    delete file with os

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-f = open("bahar.txt", "w")
-f.write("Woops! I have deleted the content!")
-f.close()
-
-#open and read the file after the overwriting:
-f = open("bahar.txt", "r")
-print(f.read())
+import os
+if os.path.exists("bahar.txt"):
+  os.remove("bahar.txt")
+else:
+  print("The file does not exist")
